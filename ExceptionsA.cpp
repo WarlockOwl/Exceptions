48da38776809f8701c2865f36f035102c64258d1
Aaron Richardson <aaron.richardson1@sinclair.edu>
2024-12-07 12:07:17 -0500

    added full functionality to the program, and comments

--- ExceptionsA.cpp
+++ ExceptionsA.cpp
@@ -10,21 +10,22 @@ using namespace std;
 char character(char, int);
 
 int main() {
-	char start, end;
-	int offset;
 	try {
-		cout << character('a', 1);
-		cout << character('a', -1);
-		cout << character('Z', -1);
-		cout << character('?', 5);
-	}
-	catch(string charExc){
-		cout << charExc;
+		cout << character('a', 1) << endl;
+		cout << character('a', -1) << endl;
 	}
 	catch (string rangeExc) {
-		cout << rangeExc;
+		cout << rangeExc << endl;
 	}
-
+	//returns an error for if the result is out of bounds of the alphabet
+	try {
+		cout << character('Z', -1) << endl;
+		cout << character('?', 5) << endl;
+	}
+	catch(string charExc){
+		cout << charExc << endl;
+	}
+	//returns an error for if the result is onot a letter of the alphabet
 	return 0;
 }
 
@@ -34,11 +35,11 @@ char character(char start, int offset) {
 
 	if (isalpha(start) == false)
 		throw charExc;
-	else if (isalpha(start + offset) == false)
+	else if (isalpha(start + offset) == false || isupper(start) != isupper(start+offset))
 		throw rangeExc;
-	else if (isupper(start) != isupper(start+offset))
-		throw rangeExc;
+	//Checks whether the letter and number entered would put the result outside of
+	//an acceptable range, or if the letter is a letter
 	else
 		return (start + offset);
-
+	//returns the letter a specified number away from the beginning number
 }
