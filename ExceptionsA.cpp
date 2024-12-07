5d7e03ef0a12f0eb6b6f68071e2afa94612b736d
Aaron Richardson <aaron.richardson1@sinclair.edu>
2024-12-07 11:43:37 -0500

    First iteration, close to complete

--- ExceptionsA.cpp
+++ ExceptionsA.cpp
@@ -7,12 +7,38 @@
 
 using namespace std;
 
+char character(char, int);
+
 int main() {
+	char start, end;
+	int offset;
+	try {
+		cout << character('a', 1);
+		cout << character('a', -1);
+		cout << character('Z', -1);
+		cout << character('?', 5);
+	}
+	catch(string charExc){
+		cout << charExc;
+	}
+	catch (string rangeExc) {
+		cout << rangeExc;
+	}
 
-
-
-
-
-
+	return 0;
 }
 
+char character(char start, int offset) {
+	string charExc = "invalidCharacterException";
+	string rangeExc = "invalidRangeException";
+
+	if (isalpha(start) == false)
+		throw charExc;
+	else if (isalpha(start + offset) == false)
+		throw rangeExc;
+	else if (isupper(start) != isupper(start+offset))
+		throw rangeExc;
+	else
+		return (start + offset);
+
+}
